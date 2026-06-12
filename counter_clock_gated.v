e25936cc8449172435505392594791d9e30ef26a
rollsroyce0143-arch <rollsroyce0143@gmail.com>
2026-06-12 23:01:27 +0530

    Refactor counter_clock_gated module and testbench

--- counter_clock_gated.v
+++ counter_clock_gated.v
@@ -1,31 +1,20 @@
-module counter_clock_gated_tb;
-
-reg clk;
-reg rst;
-reg enable;
-wire [3:0] q;
-
-counter_clock_gated uut(
-    .clk(clk),
-    .rst(rst),
-    .enable(enable),
-    .q(q)
+module counter_clock_gated(
+    input clk,
+    input rst,
+    input enable,
+    output reg [3:0] q
 );
 
-always #5 clk = ~clk;
+wire gated_clk;
 
-initial begin
-    clk = 0;
-    rst = 1;
-    enable = 1;
+assign gated_clk = clk & enable;
 
-    #10 rst = 0;
-
-    #50 enable = 0;   // Counter stops
-
-    #50 enable = 1;   // Counter starts again
-
-    #100 $finish;
+always @(posedge gated_clk or posedge rst)
+begin
+    if(rst)
+        q <= 4'b0000;
+    else
+        q <= q + 1;
 end
 
 endmodule
